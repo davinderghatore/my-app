a832031d89581c3cd2b41afed36a9e93300928d4
davinderghatore <davinders2345@gmail.com>
2025-10-03 13:08:35 +0530

    tododone

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React, { useState } from "react";
+import { useState } from 'react'; 
 
 function App() {
-  const [ToDolist,setToDolist]=useState([]);
-  const saveToDoList=(e)=> {
-    e.preventDefault(); 
-    const project= e.target.project.value;
-    if (!ToDolist.includes(project)) {
-      setToDolist([...ToDolist, project]);
-      e.target.project.value = "";
-    }
-    else{
-      alert("todo name allready exists  "); 
+
+  let [todolist,setToDolist]=useState([]);
+  let [showinput, setShowInput] = useState(false);
+ 
+  let saveToDoList=(event) => {
     
+  let project=event.target.project.value;
+  if(!todolist.include(project)){
+    let finalToDoList=[...todolist,project];
+    setToDolist(finalToDoList); 
+    setShowInput(false);
   }
-  return(
-  <div className="App">
+  else{
+    alert("project name already exists");   
+    return;
+  }
+    event.preventDefault();
+  }
+  let list=todolist.map((value,index)=>{
+    
+    return(
+    <ToDolistitems value={value} key={index} indexnumber={index} 
+    todolist={todolist}
+    setToDolist={todolist} 
+    /> 
+    )
+})
+  return (
+    <div className="App">   
     <h1>Task Management System</h1>
+    <button onClick={()=> setShowInput(!showinput)}>
+      {showinput?"cancel":"Add project"}
+
+    </button>
     <form onSubmit={saveToDoList}>
-      <input type="text" name='project' /> <button>Add Task</button>  
+      <input type="text" name='project' placeholder="Add a new task" />
+      <button type="submit">Add</button>  
     </form>
-  </div>
-  )
-}
+    <ul>
+      {list}
+      </ul>
+    </div>
+  );
 }
 
-export default App;
+export default App; 
+
+function ToDolistitems({value,indexnumber,todolist,setToDolist}){
+
+  let deleteRow=()=>{ 
+    let finaldata=todolist.filter((v,i)=>i!==indexnumber);
+    setToDolist(finaldata)
+  }
+  return(
+    <li> {indexnumber+1} {value} <span onclick={deleteRow}> &time; </span></li>
+  )
+}
